9732d121a437e3aebb6a5b918b67aa5bac6fc30c
hhbb111 <moahanaa3@gmail.com>
2025-02-17 21:12:17 +0700

    Add files via upload

---
+++ close emu.bat
@@ -0,0 +1,70 @@
+taskkill /F /IM Auxillary.exe
+taskkill /F /IM TP3Helper.exe
+taskkill /F /IM tp3helper.dat
+taskkill /f /im AndroidEmulatorEx.exe
+taskkill /f /im AndroidEmulatorEn.exe
+TaskKill /F /IM androidemulator.exe
+TaskKill /F /IM aow_exe.exe
+TaskKill /F /IM QMEmulatorService.exe
+TaskKill /F /IM RuntimeBroker.exe
+taskkill /f /im adb.exe
+taskkill /f /im GameLoader.exe
+taskkill /f /im TBSWebRenderer.exe
+taskkill /f /im AppMarket.exe
+taskkill /f /im AndroidEmulator.exe
+taskkill /f /im ninja.vmp.exe
+net stop QMEmulatorService
+net stop aow_drv
+net stop aow_exe
+net stop aow_drv_x64_ev
+net stop AndroidEmulator
+net stop aow_drv_x64
+net stop Tensafe
+taskkill /F /IM Auxillary.exe
+taskkill /F /IM TP3Helper.exe
+taskkill /F /IM tp3helper.dat
+TaskKill /F /IM androidemulator.exe
+TaskKill /F /IM aow_exe.exe
+TaskKill /F /IM QMEmulatorService.exe
+taskkill /F /IM AndroidEmulatorEx.exe
+TaskKill /F /IM RuntimeBroker.exe
+taskkill /f /im adb.exe
+taskkill /f /im GameLoader.exe
+taskkill /f /im TBSWebRenderer.exe
+taskkill /f /im AppMarket.exe
+taskkill /f /im AndroidEmulatorEX.exe
+taskkill /f /im ninja.vmp.exe
+net stop QMEmulatorService
+net stop aow_drv
+net stop aow_exe
+net stop aow_drv_x64_ev
+net stop AndroidEmulator
+net stop aow_drv_x64
+net stop Tensafe
+taskkill /F /IM Auxillary.exe
+taskkill /F /IM TP3Helper.exe
+taskkill /F /IM tp3helper.dat
+TaskKill /F /IM androidemulator.exe
+TaskKill /F /IM aow_exe.exe
+TaskKill /F /IM QMEmulatorService.exe
+TaskKill /F /IM RuntimeBroker.exe
+taskkill /f /im adb.exe
+taskkill /f /im GameLoader.exe
+taskkill /f /im TBSWebRenderer.exe
+taskkill /f /im AppMarket.exe
+taskkill /f /im AndroidEmulator.exe
+taskkill /f /im ninja.vmp.exe
+net stop QMEmulatorService
+net stop aow_drv
+net stop aow_exe
+net stop aow_drv_x64_ev
+net stop AndroidEmulator
+net stop aow_drv_x64
+net stop Tensafe
+taskkill /f /im syzs_dl_svr.exe
+taskkill /f /im TUpdate.exe
+
+
+
+
+
